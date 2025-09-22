23d8e02abee22cc682247eff2914df22c5c41f3a
GSN CREATIVE <gsn.creative6437@gmail.com>
2025-09-22 23:58:24 +0000

    Error: useSidebar must be used within a SidebarProvider.     at useSideb

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -1,13 +1,9 @@
-import { SidebarInset, SidebarProvider } from '@/components/ui/sidebar';
 import { SidebarWrapper } from './sidebar-wrapper';
 
 export default function AppLayout({ children }: { children: React.ReactNode }) {
   return (
-    <SidebarProvider defaultOpen={false}>
-      <SidebarWrapper />
-      <SidebarInset>
-        <main className="p-4 sm:p-6 lg:p-8">{children}</main>
-      </SidebarInset>
-    </SidebarProvider>
+      <SidebarWrapper>
+          {children}
+      </SidebarWrapper>
   );
 }
--- src/app/(app)/sidebar-wrapper.tsx
+++ src/app/(app)/sidebar-wrapper.tsx
@@ -6,21 +6,28 @@ import {
   SidebarContent,
   SidebarFooter,
   SidebarTrigger,
+  SidebarProvider,
+  SidebarInset,
 } from '@/components/ui/sidebar';
 import { Logo } from '@/components/logo';
 import { SidebarNavigation } from './sidebar-navigation';
 
-export function SidebarWrapper() {
+export function SidebarWrapper({ children }: { children: React.ReactNode }) {
   return (
-    <Sidebar collapsible="icon">
-      <SidebarHeader>
-        <Logo />
-        <SidebarTrigger />
-      </SidebarHeader>
-      <SidebarContent>
-        <SidebarNavigation />
-      </SidebarContent>
-      <SidebarFooter>{/* Footer content can be added here */}</SidebarFooter>
-    </Sidebar>
+    <SidebarProvider defaultOpen={false}>
+      <Sidebar collapsible="icon">
+        <SidebarHeader>
+          <Logo />
+          <SidebarTrigger />
+        </SidebarHeader>
+        <SidebarContent>
+          <SidebarNavigation />
+        </SidebarContent>
+        <SidebarFooter>{/* Footer content can be added here */}</SidebarFooter>
+      </Sidebar>
+      <SidebarInset>
+        <main className="p-4 sm:p-6 lg:p-8">{children}</main>
+      </SidebarInset>
+    </SidebarProvider>
   );
 }
