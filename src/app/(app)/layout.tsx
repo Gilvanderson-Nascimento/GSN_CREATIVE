74c9a59ddda18927ea4054255432c96b42c6a2f4
GSN CREATIVE <gsn.creative6437@gmail.com>
2025-09-23 22:19:37 +0000

    Ainda não corrigiu o erro, quro que tome as medidas necessarias para cor

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -1,16 +1,13 @@
 import { DataProvider } from '@/providers/data-provider';
-import { AuthProvider } from '@/providers/auth-provider';
 import { SidebarWrapper } from './sidebar-wrapper';
 import { PrivateRoute } from '@/components/shared/private-route';
 
 export default function AppLayout({ children }: { children: React.ReactNode }) {
   return (
     <DataProvider>
-      <AuthProvider>
-        <PrivateRoute>
-          <SidebarWrapper>{children}</SidebarWrapper>
-        </PrivateRoute>
-      </AuthProvider>
+      <PrivateRoute>
+        <SidebarWrapper>{children}</SidebarWrapper>
+      </PrivateRoute>
     </DataProvider>
   );
 }
