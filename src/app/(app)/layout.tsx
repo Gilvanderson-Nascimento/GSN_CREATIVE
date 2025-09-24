478cb5e2e27458d441f338b8cb475c453b5c32ba
GSN CREATIVE <gsn.creative6437@gmail.com>
2025-09-24 22:03:33 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -1,13 +1,10 @@
 import { SidebarWrapper } from './sidebar-wrapper';
-import { PrivateRoute } from '@/components/shared/private-route';
 
 export default function AppLayout({ children }: { children: React.ReactNode }) {
   return (
-    <PrivateRoute>
-        <div className="bg-gray-50 text-gray-800 font-sans dark:bg-background">
-            <SidebarWrapper>{children}</SidebarWrapper>
-        </div>
-    </PrivateRoute>
+    <div className="bg-gray-50 text-gray-800 font-sans dark:bg-background">
+        <SidebarWrapper>{children}</SidebarWrapper>
+    </div>
   );
 }
 
