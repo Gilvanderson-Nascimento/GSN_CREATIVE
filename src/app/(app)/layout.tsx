f8837d7cc081c45ccc291ba16e56d2f85837b1c6
GSN CREATIVE <gsn.creative6437@gmail.com>
2025-09-25 23:12:51 +0000

    Configure o login para sempre ter que fazer, com seu usuario e senha ass

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -1,11 +1,12 @@
+import { PrivateRoute } from '@/components/shared/private-route';
 import { SidebarWrapper } from './sidebar-wrapper';
 
 export default function AppLayout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="bg-gray-50 text-gray-800 font-sans dark:bg-background">
-        <SidebarWrapper>{children}</SidebarWrapper>
-    </div>
+    <PrivateRoute>
+        <div className="bg-gray-50 text-gray-800 font-sans dark:bg-background">
+            <SidebarWrapper>{children}</SidebarWrapper>
+        </div>
+    </PrivateRoute>
   );
 }
-
-    
