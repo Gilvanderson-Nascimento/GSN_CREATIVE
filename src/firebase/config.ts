bcae77e6ae215b7dd9e234549e071a1d191671f5
GSN CREATIVE <gsn.creative6437@gmail.com>
2025-09-26 01:20:44 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-3254945324-e3fd2",
+  "appId": "1:821611527451:web:ac3e4c448bcd39ac42a7be",
+  "apiKey": "AIzaSyBrQz7ZkwhIZPVpS7r4gzp9452Y539P0mU",
+  "authDomain": "studio-3254945324-e3fd2.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "821611527451"
+};
