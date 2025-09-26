70830cbe4c407e1e58e43e4599c714a654f920a6
GSN CREATIVE <gsn.creative6437@gmail.com>
2025-09-26 00:57:10 +0000

    Adicione essa imagem de logo para todo o sistema onde tem o nome do sist

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -1,10 +1,9 @@
-import { ShoppingBasket } from 'lucide-react';
-import type { SVGProps } from 'react';
+import Image from 'next/image';
 
-export function Logo(props: SVGProps<SVGSVGElement>) {
+export function Logo() {
   return (
-    <div className="flex items-center gap-2" >
-        <ShoppingBasket className="h-6 w-6 text-primary" />
+    <div className="flex items-center gap-2">
+        <Image src="/logo.png" alt="GSN Gestor Logo" width={36} height={36} />
         <h1 className="text-lg font-bold text-foreground group-data-[collapsible=icon]:hidden duration-200 ease-linear">
             GSN_GESTOR
         </h1>
